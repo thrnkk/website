53df4a496419ec1655f465124db17b792e746de3
thrnkk <thomas.ricardo.reinke@gmail.com>
2021-12-25 16:39:58 -0300

    changed to 20% change and changed css

--- script.js
+++ script.js
@@ -10,57 +10,69 @@ $(document).ready(() => {
 
         splitName = name.split('');
 
+        // for each letter
         for (let index = 0; index < splitName.length; index++) {
 
             if (splitName[index] == ' ') {
                 continue;
+
             } else {
 
-                promisesArray.push(teste(index, splitName[index]).then((response) => {
-                    splitName[response['position']] = response['original'];
-                }));
+                // 20% chance to glitch that letter
+                if (Math.random() > 0.8) {
 
+                    promisesArray.push(randomizeChar(index, splitName[index]).then((response) => {
+                        splitName[response['position']] = response['original'];
+                    }));
+
+                }
             }
         }
 
         Promise.all(promisesArray).then(() => {
-
-            $('#glitch').text(name);
-
+            $('#glitch').html(setClasses(name));
         });
 
     }, 5000);
 
-    async function teste(position, originalChar) {
+    $('#glitch').html(setClasses(name));
 
+    async function randomizeChar(position, originalChar) {
+
+        // random delay
         await new Promise(resolve => setTimeout(resolve, Math.floor(Math.random() * (2000))));
 
-        return new Promise((resolve, reject) => {
+        return new Promise((resolve) => {
 
             let count = 0;
             let limit = Math.floor(Math.random() * (100)) + 10;
-            let speed = 0;
             let interval = setInterval(() => {
     
                 if (count >= limit) {
                     resolve({'position': position, 'original': originalChar});
-                    clearInterval(interval);
-                    
+                    clearInterval(interval)
                 }
 
                 let newChar = strangeChars[strangeChars.length * Math.random() | 0];
                 splitName[position] = newChar;
 
-                $('#glitch').text(splitName.join(''));
-
+                $('#glitch').html(setClasses(splitName.join('')));
                 count++;
         
             }, 10);
         });
     }
 
-    function delay(ms) {
-        return 
+    // set even space between letters
+    function setClasses(name) {
+
+        nameList = name.split('');
+
+        for (let i = 0; i < nameList.length; i++) {
+            nameList[i] = '<span class="letter">' + nameList[i] + '</span>';
+        }
+
+        return nameList.join('');
     }
 
 });
